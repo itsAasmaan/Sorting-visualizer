7617b0f4e4751b9555d27c3b55003739c209f423
itsAasmaan <pariharakashsingh18@gmail.com>
2026-03-21 23:52:55 +0530

    feat: Added Insertion Sort algorithm

--- src/SortingVisualizer.js
+++ src/SortingVisualizer.js
@@ -50,6 +50,7 @@ const SortingVisualizer = () => {
           <select value={selectedAlgorithm} onChange={(e) => setSelectedAlgorithm(e.target.value)} disabled={isSorting}>
             <option value="bubbleSort">Bubble Sort</option>
             <option value="selectionSort">Selection Sort</option>
+            <option value="insertionSort">Insertion Sort</option>
           </select>
         </div>
 
@@ -72,7 +73,7 @@ const SortingVisualizer = () => {
             <option value="30">Fast</option>
             <option value="50">Normal</option>
             <option value="100">Slow</option>
-            <option value="200">Very Slow</option>
+            <option value="500">Very Slow</option>
           </select>
         </div>
 
@@ -81,7 +82,7 @@ const SortingVisualizer = () => {
         <button className="primary" onClick={handleSorting} disabled={isSorting}>
           Sort!
         </button>
-        <button className="secondary-btn" onClick={() => generateArray(size)} disabled={isSorting}>
+        <button className="reset-btn" onClick={() => generateArray(size)} disabled={isSorting}>
           Reset
         </button>
       </div>
